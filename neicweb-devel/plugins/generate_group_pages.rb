631a79d8d436aea7d8bf0da558cfedbe7123dc67
Joel Hedlund <joel@nsc.liu.se>
2015-11-28 23:18:02 +0100

    Move json contact info to /int/api/contact/

--- neicweb-devel/plugins/generate_group_pages.rb
+++ neicweb-devel/plugins/generate_group_pages.rb
@@ -16,11 +16,11 @@ module Jekyll
   end
   
   class JsonGroupPage < Page
-    def initialize(site, base, dir, name, id)
+    def initialize(site, base, dir, id)
       @site = site
       @base = base
       @dir = dir
-      @name = name
+      @name = 'index.html'
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'group.json')
@@ -34,6 +34,7 @@ module Jekyll
     def generate(site)
       if site.layouts.key? 'group'
         dir = site.config['group_dir'] || 'about/team'
+        api_dir = site.config['contact_api_dir'] || 'int/api/contact'
         existing = []
         site.pages.each do |page|
           if match = page.url.match(%r{^/#{dir}/([^/]+)/[^/]*$})
@@ -46,7 +47,7 @@ module Jekyll
             title = site.data['groups'][group_id]['name'] || group_id
             menu = site.data['groups'][group_id]['menu'] || 100
             site.pages << GroupPage.new(site, site.source, File.join(dir, group_id), title, menu, group_id)
-            site.pages << JsonGroupPage.new(site, site.source, dir, group_id + '.json', group_id)
+            site.pages << JsonGroupPage.new(site, site.source, File.join(api_dir, group_id), group_id)
             existing << group_id
           end
         end
